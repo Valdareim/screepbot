00abd63315458c443d1fec1b06becaa8c80b40f0
Angel Bouzo <zerok60@gmail.com>
2018-04-13 20:49:29 -0700

    and undefined

--- src/spawner.js
+++ src/spawner.js
@@ -94,7 +94,7 @@ var spawner = {
             const target = spawn.pos.findClosestByRange(FIND_CREEPS, {
                 filter: object => object.ticksToLive < 1400
             });
-            if (target !== null) {
+            if (target !== null && target !== undefined) {
                 spawn.room.visual.text(`Renewing ${target.variant} - ${target.role}`);
                 spawn.renewCreep(target);
             }
